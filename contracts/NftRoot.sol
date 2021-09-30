1b2f265f96e56171c5c32ca808e28493d8eaafe0
rightjelkin <romgalkin23@gmail.com>
2021-09-30 15:42:07 +0700

    feat: mintNft method now returns new NFT id

--- contracts/NftRoot.sol
+++ contracts/NftRoot.sol
@@ -25,11 +25,11 @@ contract NftRoot is DataResolver, IndexResolver, InternalOwner {
         owner = internalOwner;
     }
 
-    function mintNft() public onlyOwner {
+    function mintNft() public onlyOwner returns (uint256 id) {
         TvmCell codeData = _buildDataCode(address(this));
         TvmCell stateData = _buildDataState(codeData, _totalMinted);
         new Data{stateInit: stateData, value: 1.1 ton}(msg.sender, _codeIndex);
-
+        id = _totalMinted;
         _totalMinted++;
     }
 
